a955d8069318915717e09fa8a719e79d60e66715
Jinyan <jinyan.shen21@gmail.com>
2018-09-24 18:06:44 -0400

    second commit

--- models.js
+++ models.js
@@ -2,5 +2,34 @@ const Sequelize = require('sequelize');
 
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
+const Gardener = db.define('gardener', {
+    name: Sequelize.STRING,
+    age: Sequelize.INTEGER,
+
+})
+
+const Plot = db.define('plot', {
+    size: Sequelize.INTEGER,
+    shaded: Sequelize.BOOLEAN
+})
+
+const Vegetable = db.define('vegetable', {
+    name: Sequelize.STRING,
+    color: Sequelize.STRING,
+    planted_on: Sequelize.DATE
+})
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
+Vegetable.belongsToMany(Plot, {through: 'relationship'});
+Plot.belongsToMany(Vegetable, {through: 'relationship'});
+
+
+
+
+
+
+
 module.exports = db;
 
